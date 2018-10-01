9f3293fa32fc87cac4393a88c8363ae31084f0cb
Nick Keil <nicolaskeil.school@gmail.com>
2018-09-30 21:27:02 -0700

    modules folder

---
+++ Modules/IAM.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Discord_BodmodBot.Modules
+{
+    public class IAM : ModuleBase<SocketCommandContext>
+    {
+        Random _ran = new Random();
+
+        // Gives user Tan-Ultra (mute role)
+        [Command("tanUltra"), RequireBotPermission(GuildPermission.ManageRoles)]
+        public async Task TanUltra(SocketGuildUser arg, [Remainder]string reason = null)
+        {
+            await Context.Message.DeleteAsync();
+
+            var tanUltraRole = Context.Guild.Roles.FirstOrDefault(y => y.Name == "Tan-Ultra (Muted)");
+            await (arg as IGuildUser).AddRoleAsync(tanUltraRole);
+
+            EmbedBuilder builder = new EmbedBuilder()
+            {
+                Title = $"**{arg.Username} has been placed under Tan-Ultra**",
+                Description = $"{(string.IsNullOrEmpty(reason) ? string.Empty : $"**Reason: {reason}**")}",
+                Color = new Color((byte)(_ran.Next(255)), (byte)(_ran.Next(255)), (byte)(_ran.Next(255)))
+            }.WithCurrentTimestamp();
+
+            await ReplyAsync(string.Empty, false, embed: builder.WithFooter(y => y.WithText($"{Context.User}")).Build());
+
+            EmbedBuilder builder2 = new EmbedBuilder()
+            {
+                Description = $"**You've been muted in {Context.Guild.Name}**{(string.IsNullOrEmpty(reason) ? string.Empty : $"\n**Reason: {reason}**")}",
+                Color = new Color((byte)(_ran.Next(255)), (byte)(_ran.Next(255)), (byte)(_ran.Next(255)))
+            }.WithCurrentTimestamp();
+
+            IDMChannel x = await arg.GetOrCreateDMChannelAsync();
+            builder.WithCurrentTimestamp();
+            await x.SendMessageAsync(string.Empty, false, embed: builder2.Build());
+        }
+    }
+}
---
+++ Modules/Misc.cs
@@ -0,0 +1,72 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Discord_BodmodBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        //Greets user
+        [Command("hi")]
+        public async Task Hi()
+        {
+            string response = "stop annoying me " + Context.User.Username;
+            await Context.Channel.SendMessageAsync(response);
+        }
+        //Picks randomly from an assortment of choices seperated by 'or'
+        [Command("pick")]
+        public async Task PickOne([Remainder]string message)
+        {
+            string[] options = message.Split(new string[] { " or " }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string selection = options[r.Next(0, options.Length)];
+
+            string responce = "i choose " + selection;
+            await Context.Channel.SendMessageAsync(responce);
+        }
+        //Gets the url of the avatar of selected user
+        [Command("avatar")]
+        public async Task Avatar(IGuildUser user)
+        {
+            string pic = user.GetAvatarUrl();
+            await Context.Channel.SendMessageAsync(pic);
+        }
+        //DM's user help menu
+        [Command("help")]
+        public async Task Help ()
+        {
+            var dmChannel = await Context.User.GetOrCreateDMChannelAsync();
+            string message = "to make me do things @ me or use `*` \ncommands: ```*help \n*hi \n*pick [choice1] or [choice2] or... \n*avatar [user name] ```";
+            await dmChannel.SendMessageAsync(message);
+        }
+        // encode w/ caesar and key
+        [Command("caesar-e")]
+        public async Task CaesarEncode(string message, string key_s)
+        {
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int key_i = Int32.Parse(key_s);
+            string encoded_string = "";
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (upperCase.Contains(message[i]))
+                    encoded_string += (char)(((message[i] + key_i - 65) % 26) + 65);
+                else if (lowerCase.Contains(message[i]))
+                    encoded_string += (char)(((message[i] + key_i - 97) % 26) + 97);
+                else
+                    encoded_string += message[i];
+            }
+           
+            await Context.Channel.SendMessageAsync(encoded_string);
+        }
+    }
+}
